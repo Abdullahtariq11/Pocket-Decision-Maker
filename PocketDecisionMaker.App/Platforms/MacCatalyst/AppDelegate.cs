32f40f88ae1882503f30827a6e2fb557a843f188
Abdullah Tariq <abdullahtariq096@gmail.com>
2024-11-30 19:46:26 -0800

    Initial Setup

---
+++ PocketDecisionMaker.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PocketDecisionMaker.App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
